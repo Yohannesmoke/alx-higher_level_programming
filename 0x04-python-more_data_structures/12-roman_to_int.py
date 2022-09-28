0d3971985af42dfbf1d703beecb07e0cc440b43e
root <yohannesmekonen091355@gmail.com>
2022-09-28 10:17:22 -0700

    hot fix 2

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    """
+    Converts a Roman numeral to an integer.
+    ...
+    Parameters
+    ----------
+    roman_string : str
+        the roman numeral to convert
+    Return:
+        0 If the roman_string is not a string or None
+        The corresponding integer otherwise.
+    """
+
     if (not isinstance(roman_string, str) or
             roman_string is None):
         return (0)
