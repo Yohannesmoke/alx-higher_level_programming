38924eb224762cab6eb677f897f526ce27f4a180
root <yohannesmekonen091355@gmail.com>
2022-10-12 06:12:39 -0700

    commit 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""this will determine the square of a circle"""
+
+class Square:
+    """a class that have a size attriute which is private"""
+    def __init__(self,size):
+        self.__size = size
