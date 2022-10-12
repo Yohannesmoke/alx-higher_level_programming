0cba4107d27e54bb18a0fb832e6653bcaae5cf9c
root <yohannesmekonen091355@gmail.com>
2022-10-12 06:21:00 -0700

    code fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,5 +4,10 @@
 
 class Square:
     """a class that have a size attriute which is private"""
-    def __init__(self,size):
+   def __init__(self,size):
+       """initializes a square 
+       Args:
+       size (int): size of a side of the square
+       Reaturns: None
+       """
         self.__size = size
