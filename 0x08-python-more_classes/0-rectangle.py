9e720cdb9fed6916d90ff6839ff57c6ea8676661
root <yohannesmekonen091355@gmail.com>
2022-10-17 09:52:09 -0700

    the first commit

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-
+"""a program that has a class and don't have a test case """
 class Rectangle:
+    """an empty class that doesn't do any thing 
+    just an empty class"""
     pass
