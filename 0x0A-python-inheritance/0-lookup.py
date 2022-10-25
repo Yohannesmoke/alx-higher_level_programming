d2382a2a46a48dcdd99606a7f02a834961aca8cb
root <yohannesmekonen091355@gmail.com>
2022-10-25 06:54:18 -0700

    first commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""this is a function that will print the list 
+of available attributes and methods of an object"""
+def lookup(obj):
+    """"inside a function that will print list"""
+    print(dir(obj))
