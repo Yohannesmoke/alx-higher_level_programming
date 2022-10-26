2d85e6cc1bd16590992f9c6225339863f7d5c511
root <yohannesmekonen091355@gmail.com>
2022-10-26 09:48:28 -0700

    the fifth commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Write a class BaseGeometry (based on 5-base_geometry.py)."""
+class BaseGeometry:
+    """just a class with one method"""
+    def area(self):
+        """that raises an Exception with the message """
+        raise Exception("area() is not implemented")
