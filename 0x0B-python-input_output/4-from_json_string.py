1f772f560d26f5640dfa7f70cb6a7ec547b25166
root <yohannesmekonen091355@gmail.com>
2022-10-31 09:25:28 -0700

    third commit

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""returns an object (Python data structure) represented """
+import json
+def from_json_string(my_str):
+    """
+    an object (Python data structure)
+     represented by a JSON string:
+     """
+    return json.loads(my_str)
