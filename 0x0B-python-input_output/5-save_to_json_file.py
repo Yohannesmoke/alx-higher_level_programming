c687e04a87065d44d7b10b32aa48d0223a64cb19
root <yohannesmekonen091355@gmail.com>
2022-10-31 09:40:37 -0700

    third commit

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" 
+a function that writes an Object to a text file, 
+using a JSON representation:
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ 
+    a function that writes an Object to a text
+     file, using a JSON representation:
+     """
+    with open(filename, "w",encoding="utf8") as fi:
+        fi.write(dumps(my_obj))
