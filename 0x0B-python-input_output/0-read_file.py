df15c7b7886255250100db26e676c26ae40eb494
root <yohannesmekonen091355@gmail.com>
2022-10-31 07:32:36 -0700

    the first commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,7 @@
  (UTF8) and prints it to stdout:"""
 def read_file(filename=""):
     """Write a function that reads a text file"""
-    with open(filename, encoding="utf-8") as my_file:
-        my_file.read()
+    with open(filename,mode = 'r',encoding="utf-8") as my_file:
+        for i in my_file:
+            print(i, end="")
+    my_file.closed
