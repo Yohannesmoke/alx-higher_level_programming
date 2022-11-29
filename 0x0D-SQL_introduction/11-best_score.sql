11d2ed2861eae51787d8a4c2e6f37250af29522d
root <yohannesmekonen091355@gmail.com>
2022-11-29 10:59:55 -0800

    ko

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,6 @@
 --lists all records 
 -- who are greater than score >= 10
+-- results display score and name
+-- orderd by score
+-- with score >= 10
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
