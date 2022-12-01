d77417b77ac338172d3953c9b669344e4da5b733
root <yohannesmekonen091355@gmail.com>
2022-12-01 02:51:21 -0800

    jojo

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 -- name is user_0d_1
 -- all privilages will be given to it 
 -- the passward should be user_0d_1_pwd
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
