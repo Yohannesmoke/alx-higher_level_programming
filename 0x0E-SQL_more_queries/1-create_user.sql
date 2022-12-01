cfa6f91463276680ae72954a47744688c901bcc5
root <yohannesmekonen091355@gmail.com>
2022-12-01 02:49:17 -0800

    jojo

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- create user in mysql
+-- name is user_0d_1
+-- all privilages will be given to it 
+-- the passward should be user_0d_1_pwd
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
