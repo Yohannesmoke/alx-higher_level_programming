54514c1adb587d92bb8319dbad98932ad56b6902
root <yohannesmekonen091355@gmail.com>
2022-12-01 04:01:16 -0800

    jojo

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,4 +3,4 @@
 -- all privilages will be given to it 
 -- the passward should be user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
