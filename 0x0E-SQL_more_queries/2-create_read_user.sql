44b07af2adfad5fbf0be9056c9bf90488b1dffdb
root <yohannesmekonen091355@gmail.com>
2022-12-01 04:32:15 -0800

    jojojo

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- create database hbtn_0d_2
+-- user user_0d_2 and password user_0d)2_pwd
+-- privilage select
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+USE hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON *.* TO user_0d_2;
